5de1195ce0a4d928238c22dcc9c3d3f001f5e1e4
fang-zhiyou <63876026+fang-zhiyou@users.noreply.github.com>
2023-06-02 18:24:04 +0800

    Update Persistence.swift

--- iOS/CoreData/Persistence.swift
+++ iOS/CoreData/Persistence.swift
@@ -28,7 +28,9 @@ struct PersistenceController {
         }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
+                
                 fatalError("Unresolved error \(error), \(error.userInfo)")
+                
             }
         })
     }
